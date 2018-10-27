04137cb8cbaee3901ce8b4d7ea3971f5cc5c2e72
joker <18757883747@163.com>
2018-10-27 17:42:31 +0800

    first add

---
+++ library/src/main/java/com/joker/library/sqlextention/service/abs/AbstractSQLExtentionProxyBaseCRUDService.java
@@ -0,0 +1,24 @@
+/**
+*
+* @Description
+* @author joker 
+* @date 创建时间：2018年10月27日 下午1:03:31
+* 
+*/
+package com.joker.library.sqlextention.service.abs;
+
+import com.joker.library.sqlextention.AbstractSQLExtentionModel;
+import com.joker.library.sqlextention.service.ISQLExtentionBaseCRUDService;
+
+/**
+* 
+* @When
+* @Description
+* @Detail
+* @author joker 
+* @date 创建时间：2018年10月27日 下午1:03:31
+*/
+public abstract class AbstractSQLExtentionProxyBaseCRUDService<T extends AbstractSQLExtentionModel> implements ISQLExtentionBaseCRUDService<T>
+{
+	
+}
